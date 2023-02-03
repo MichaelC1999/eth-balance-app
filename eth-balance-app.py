0132edae587d314142e50a1d6ce91e542d02c13a
Michael <michaelcarroll1999@gmail.com>
2023-02-03 19:07:09 -0300

    Add max block and filter address

--- eth-balance-app.py
+++ eth-balance-app.py
@@ -6,6 +6,7 @@ from tempfile import NamedTemporaryFile
 import pandas as pd
 import substreams as sub
 import os
+import sys
 import threading
 import time
 import requests
@@ -15,22 +16,53 @@ import types
 from dotenv import load_dotenv
 load_dotenv()
 
+sys.setrecursionlimit(15000)
 
 st.set_page_config(layout='wide')
+style_css = """
+    <style>
+        div.table-container {
+            width: 100%;
+            overflow: scroll;
+        }
+        table.dataframe {
+        width: 100%;
+        background-color: rgb(35,58,79);
+        border-collapse: collapse;
+        border-width: 2px;
+        border-color: rgb(17,29,40);
+        border-style: solid;
+        color: white;
+        font-size: 14px;
+        }
+        table.dataframe td, table.dataframe th {
+        text-align: left;
+        border-top: 2px rgb(17,29,40) solid;
+        border-bottom: 2px rgb(17,29,40) solid;
+        padding: 3px;
+        white-space:nowrap;
+        }
+        table.dataframe thead {
+            color: rgb(215,215,215);
+        background-color: rgb(17,29,40);
+        }
+    </style>"""
 sb = None
 sb_keys = []
 
 if bool(st.session_state) is False:
     st.session_state['streamed_data'] = []
-    st.session_state['retruned_block_numbers'] = []
-    st.session_state['highest_processed_block'] = 0    
     st.session_state['attempt_failures'] = 0
     st.session_state['error_message'] = ""
     st.session_state['has_started'] = False
     st.session_state["sftoken"] = None
+    st.session_state["min_block"] = 10000000
     st.experimental_rerun()
 
-block_start = st.number_input('START BLOCK:', min_value=10000000, max_value=20000001)
+block_start = st.number_input('START BLOCK:', min_value=1, max_value=20000001)
+block_end = st.number_input('END BLOCK:', min_value=2, max_value=20000001, key="max_block")
+
+st.text_input('FILTER ADDRESS:', placeholder="0x0", key="filter_address")
 
 stop_message = st.empty()
 if 'has_started' in st.session_state:
@@ -64,13 +96,16 @@ elif "APIKEY" in os.environ and st.session_state["sftoken"] is None:
 sb = Substream('./eth-balance-v0.1.0.spkg', token=st.session_state["sftoken"])
 
     
-min_block = 10000000
+min_block = block_start
 if 'min_block' in st.session_state:
     min_block = st.session_state['min_block']
 
-filter_address = "8b695d0d7160aa8d95dc6ccef6e7133f76a91de7"
+# filter_address = "8b695d0d7160aa8d95dc6ccef6e7133f76a91de7"
     
-max_block = 20000000
+max_block = block_end
+if 'max_block' in st.session_state:
+    max_block = st.session_state['max_block']
+
 
 placeholder = st.empty()
 
@@ -78,8 +113,7 @@ def get_row_with_balance(row):
     try:
         new_balance = None
         for x in row["balanceChanges"]:
-            print(x["address"], 'a')
-            if x["address"] == filter_address:
+            if "0x" + x["address"] == st.session_state["filter_address"].lower():
                 new_balance = x["newBalance"]
     except Exception as err:
         print("ERROR2 --- ", err)
@@ -91,8 +125,12 @@ def get_row_with_balance(row):
     return_dict["block"] = row["blockNumber"]
     return_dict["tx"] = row["txHash"]
     return_dict["new_balance"] = new_balance
-    return_dict["address"] = filter_address
+    return_dict["address"] = st.session_state["filter_address"].lower()
     return return_dict
+
+def get_valid_rows(data_rows):
+    valid_tx = list(filter(lambda d: "0x" + d['to'] == st.session_state["filter_address"].lower() or "0x" + d["from"] == st.session_state["filter_address"].lower(), data_rows))
+    return len(valid_tx) > 0
 
 if 'streamed_data' in st.session_state:
     if len(st.session_state['streamed_data']) > 0:
@@ -104,40 +142,7 @@ if 'streamed_data' in st.session_state:
             copy_df = copy_df.sort_values(by=st.session_state['rank_col'],ascending=False)
             copy_df.index = range(1, len(copy_df) + 1)
             copy_df['tx'] = '0x' + copy_df['tx'].astype(str)
-            # copy_df['poolAddress'] = '0x' + copy_df['poolAddress'].astype(str)
-            # copy_df['sender'] = '0x' + copy_df['sender'].astype(str)
         html_table = '<div class="table-container">' + copy_df[:500].to_html() + '</div>'
-        style_css = """
-                <style>
-                    div.table-container {
-                        width: 100%;
-                        overflow: scroll;
-                    }
-
-                    table.dataframe {
-                    width: 100%;
-                    background-color: rgb(35,58,79);
-                    border-collapse: collapse;
-                    border-width: 2px;
-                    border-color: rgb(17,29,40);
-                    border-style: solid;
-                    color: white;
-                    font-size: 14px;
-                    }
-
-                    table.dataframe td, table.dataframe th {
-                    text-align: left;
-                    border-top: 2px rgb(17,29,40) solid;
-                    border-bottom: 2px rgb(17,29,40) solid;
-                    padding: 3px;
-                    white-space:nowrap;
-                    }
-
-                    table.dataframe thead {
-                        color: rgb(215,215,215);
-                    background-color: rgb(17,29,40);
-                    }
-                </style>"""
         st.markdown(style_css + html_table, unsafe_allow_html=True)
 
 error_message = st.empty()
@@ -146,12 +151,13 @@ if 'error_message' in st.session_state:
     if st.session_state['error_message'] != "" and st.session_state['error_message'] is not None: 
         error_message.text(st.session_state['error_message'])
 
-if st.session_state['has_started'] is True:
+if st.session_state['has_started'] is True and "filter_address" in st.session_state:
     st.session_state['error_message'] = ""
     if 'min_block' in st.session_state:
-        # If min_block is saved in state, override the min_block from the UI input
         min_block = st.session_state['min_block']
     if min_block > 0:
+        if st.session_state["filter_address"] is None or st.session_state["filter_address"] == "":
+            raise TypeError('No address provided for filter.')
         if max_block < min_block:
             raise TypeError('`min_block` is greater than `max_block`. This cannot be validly polled.')
         if max_block == min_block:
@@ -161,9 +167,8 @@ if st.session_state['has_started'] is True:
         if max_block > min_block and sb is not None:
             poll_return = {}
             try:
-                print('boutta rip', min_block, len(st.session_state['streamed_data']))
                 placeholder.text("Loading Substream Results...")
-                poll_return = sb.poll("map_balances", start_block=min_block, end_block=max_block, return_first_result=True, return_type="dict")
+                poll_return = sb.poll("map_balances", start_block=min_block, end_block=max_block, return_first_result=True, return_type="dict", return_first_result_function=get_valid_rows)
                 placeholder.empty()
                 if "error" in poll_return:
                     if poll_return["error"] is not None:
@@ -173,7 +178,7 @@ if st.session_state['has_started'] is True:
                             raise TypeError(str(poll_return["error"]) + " BLOCK: " + str(poll_return["data_block"]))
                 if "data" in poll_return:
                     if (len(poll_return["data"]) > 0):
-                        valid_tx = list(filter(lambda d: d['to'] == filter_address or d["from"] == filter_address, poll_return["data"]))
+                        valid_tx = list(filter(lambda d: "0x" + d['to'] == st.session_state["filter_address"].lower() or "0x" + d["from"] == st.session_state["filter_address"].lower(), poll_return["data"]))
                         if (len(valid_tx) > 0):
                             valid_tx_manipulated = map(get_row_with_balance, valid_tx)
                             st.session_state['streamed_data'].extend(valid_tx_manipulated)
